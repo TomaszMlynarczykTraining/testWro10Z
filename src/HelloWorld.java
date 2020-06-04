fdd5498aebc0f5d3600d9aaea7b4050676535054
Tomok <tomasz.feliks.mlynarczyk@gmail.com>
2020-06-04 19:32:05 +0200

    init commit2

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String nextLine = scanner.nextLine();
+        System.out.println("This is an hello world" + nextLine);
+
+
+    }
+
+
+}
