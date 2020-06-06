c08c9e05cd4448fe29f0673f8f698fa97e112ef7
Tomok <tomasz.feliks.mlynarczyk@gmail.com>
2020-06-06 10:21:51 +0200

    added hello world example

---
+++ src/dzien_pierwszy_przyklady/HelloWorldExample.java
@@ -0,0 +1,19 @@
+package dzien_pierwszy_przyklady;
+
+public class HelloWorldExample {
+
+    public static void main(String[] args) {
+
+        //To jest użycie funkcji println
+        // System.out.println("Na imię mi Tomasz");
+        System.out.println("A na nazwisko Młynarczyk");
+
+        System.out.print("Jabłko");
+        System.out.println();
+        System.out.print("Owoc");
+
+        System.out.printf("To jest printf");
+
+
+    }
+}
