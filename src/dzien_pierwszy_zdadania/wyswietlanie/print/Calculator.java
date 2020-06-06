33a74ca582292ef519750c80c27f90c2e4a975d5
Tomok <tomasz.feliks.mlynarczyk@gmail.com>
2020-06-06 13:19:36 +0200

    first tasks solutions

---
+++ src/dzien_pierwszy_zdadania/wyswietlanie/print/Calculator.java
@@ -0,0 +1,7 @@
+package dzien_pierwszy_zdadania.wyswietlanie.print;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Initializing calculator...");
+    }
+}
---
+++ src/dzien_pierwszy_zdadania/wyswietlanie/print/Zadanie1_1.java
@@ -0,0 +1,25 @@
+package dzien_pierwszy_zdadania.wyswietlanie.print;
+
+public class Zadanie1_1 {
+
+    public static void main(String[] args) {
+        //1.1
+        String name = "Tomasz";
+        System.out.println("Witaj" + name + ".");
+
+        //1.2
+        System.out.print("Pierwszy ciag \n");
+        // System.out.println();
+        System.out.print("Drugi ciag \n");
+        // System.out.println();
+
+        //1.4. Wyrównanie do 15 znaków
+
+        System.out.printf("%15s", "SDAASDASD    ");
+        System.out.println();
+
+        //1.5. Wyświetlanie hexadecymalne
+        System.out.printf("%02x", 192);
+
+    }
+}
