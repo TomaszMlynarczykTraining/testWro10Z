4eb21898826b2d8f5f60f306154513f510c0b25d
Tomok <tomasz.feliks.mlynarczyk@gmail.com>
2020-06-06 11:28:18 +0200

    added variables ex

---
+++ src/dzien_pierwszy_przyklady/VariablesExample.java
@@ -0,0 +1,25 @@
+package dzien_pierwszy_przyklady;
+
+public class VariablesExample {
+
+    public static void main(String[] args) {
+        int multiplier = 10;
+        final byte myByte = 2;
+
+        double myDouble = 23.3;
+        float myFloat = 32.3f;
+
+        String myString = "dsa";
+        String asdsa = "asdsa";
+
+        char myChar = '2';
+
+        System.out.println(multiplier);
+
+        multiplier = 16;
+        System.out.println(multiplier);
+
+        System.out.println(myByte);
+
+    }
+}
