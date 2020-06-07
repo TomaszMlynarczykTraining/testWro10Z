c298e716c327b3adb298e783ddbf8632cda225a6
Tomok <tomasz.feliks.mlynarczyk@gmail.com>
2020-06-07 10:30:45 +0200

    for loop

--- src/dzien_drugi_przyklady/petle/ForExample.java
+++ src/dzien_drugi_przyklady/petle/ForExample.java
@@ -6,20 +6,28 @@ public class ForExample {
 
     public static void main(String[] args) {
 
+        int outsideVar = 5;
+
         for (int i = 0; i < 5; i++) {
             Scanner newScanner = new Scanner(System.in);
             String nextLine = newScanner.nextLine();
             System.out.println("Wartość iteratora " + i);
             System.out.println("Wartość z konsoli " + nextLine);
+
+            //zmienna zewnetrzna jest widoczna
+            outsideVar = outsideVar + i;
+            System.out.println("Wartość outside Var " + outsideVar);
         }
 
-        for (int i = 4; i <= 5; i++) {
+        System.out.println("Wartość outside Var " + outsideVar);
+
+/*        for (int i = 4; i <= 5; i++) {
             System.out.println("Wartość iteratora " + i);
         }
 
         for (int i = 10; i > 0; i--) {
             System.out.println("Wartość iteratora " + i);
-        }
+        }*/
 
 
 /*        for (int i = 4; i < 5; ++i) {
